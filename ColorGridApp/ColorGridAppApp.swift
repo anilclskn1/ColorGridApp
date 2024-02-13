2af2f1c9bd1e033ed30bdc9b06fc073c95bb3d99
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-13 22:13:41 +0300

    Initial Commit

---
+++ ColorGridApp/ColorGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGridAppApp.swift
+//  ColorGridApp
+//
+//  Created by Anil Caliskan on 13.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct ColorGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
